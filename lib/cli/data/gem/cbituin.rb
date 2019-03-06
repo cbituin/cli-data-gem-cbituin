f92e13184aa20abff729c2cb4d054acceba9e913
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-03-06 13:21:08 +0000

    initial files created

---
+++ lib/cli/data/gem/cbituin.rb
@@ -0,0 +1,11 @@
+require "cli/data/gem/cbituin/version"
+
+module Cli
+  module Data
+    module Gem
+      module Cbituin
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ spec/cli/data/gem/cbituin_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+RSpec.describe Cli::Data::Gem::Cbituin do
+  it "has a version number" do
+    expect(Cli::Data::Gem::Cbituin::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
